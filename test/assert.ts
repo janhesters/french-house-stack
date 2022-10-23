5d9d9b8433f16c5f19c07824cb5c40bf66daef7a
Jan <jan.hesters@rwth-aachen.de>
2022-10-23 14:29:50 +0200

    refactor: rewrite assert to use expect
    Rewrites assert so it uses expect.
    
    closes #49

--- test/assert.ts
+++ test/assert.ts
@@ -1,12 +1,22 @@
-import { expect, test } from 'vitest';
+import { expect } from 'vitest';
 
 interface Assertion<T> {
   /**
-   *
+   * Prose description of what is `given` to the component under test.
    */
   readonly given: string;
+  /**
+   * Prose description of what the actual result of calling the component with
+   * the given arguments `should` be.
+   */
   readonly should: string;
+  /**
+   * The actual result in the code by calling the component.
+   */
   readonly actual: T;
+  /**
+   * The expected result in the code.
+   */
   readonly expected: T;
 }
 
@@ -21,25 +31,8 @@ interface Assertion<T> {
  * @see https://medium.com/javascript-scene/what-every-unit-test-needs-f6cd34d9836d
  */
 export function assert<T>(assertion: Assertion<T>) {
-  test(`given ${assertion.given}: should ${assertion.should}`, () => {
-    expect(assertion.actual).toEqual(assertion.expected);
-  });
+  expect(
+    assertion.actual,
+    `given ${assertion.given}: should ${assertion.should}`,
+  ).toEqual(assertion.expected);
 }
-
-assert.todo = function todo<T>(assertion: Assertion<T>) {
-  test.todo(`given ${assertion.given}: should ${assertion.should}`, () => {
-    expect(assertion.actual).toEqual(assertion.expected);
-  });
-};
-
-assert.only = function only<T>(assertion: Assertion<T>) {
-  test.only(`given ${assertion.given}: should ${assertion.should}`, () => {
-    expect(assertion.actual).toEqual(assertion.expected);
-  });
-};
-
-assert.skip = function skip<T>(assertion: Assertion<T>) {
-  test.skip(`given ${assertion.given}: should ${assertion.should}`, () => {
-    expect(assertion.actual).toEqual(assertion.expected);
-  });
-};
