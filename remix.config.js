12ed53e92e61c7c3f3156761195535780ba991cf
Jan <jan.hesters@rwth-aachen.de>
2023-02-17 19:16:47 +0100

    chore: upgrade packages
    Upgrade Remix to v1.13.
    
    closes #140

--- remix.config.js
+++ remix.config.js
@@ -1,19 +1,14 @@
 /* eslint-disable unicorn/prefer-module */
-const { flatRoutes } = require('remix-flat-routes');
-
 /**
  * @type {import('@remix-run/dev').AppConfig}
  */
 module.exports = {
-  ignoredRouteFiles: ['**/*'],
-  routes: defineRoutes =>
-    flatRoutes('routes', defineRoutes, {
-      ignoredRouteFiles: ['**/*.test.{ts,tsx}'],
-    }),
+  ignoredRouteFiles: ['**/*.test.{ts,tsx}'],
   future: {
     // broken, see: https://github.com/remix-run/remix/issues/5322
-    // v2_routeConvention: true,
+    v2_routeConvention: true,
     v2_meta: true,
     v2_errorBoundary: true,
+    unstable_tailwind: true,
   },
 };
